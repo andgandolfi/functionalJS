a96cbd2c49512044ccfccf9d87b21f8b84dc93b5
Andrea Gandolfi <andgandolfi@gmail.com>
2017-04-02 13:57:03 +0200

    Add some utility functions to Either type

--- src/types.js
+++ src/types.js
@@ -8,6 +8,7 @@ const Box = x => ({
 // Right :: b -> Either a b
 const Right = x => ({
   map: f => Right(f(x)),
+  chain: f => f(x),
   fold: (f, g) => g(x),
   inspect: () => `Right(${x})`,
 })
@@ -15,12 +16,28 @@ const Right = x => ({
 // Left :: a -> Either a b
 const Left = x => ({
   map: f => Left(x),
+  chain: f => Left(x),
   fold: (f, g) => f(x),
   inspect: () => `Left(${x})`,
 })
+
+// fromNullable :: b -> a -> Either a b
+const fromNullable = (x, err = null) =>
+  x != null ? Right(x) : Left(err)
+
+// tryCatch :: (() -> a) -> Either b a
+const tryCatch = f => {
+  try {
+    return Right(f())
+  } catch (e) {
+    return Left(e)
+  }
+}
 
 module.exports = {
   Box,
   Left,
   Right,
+  fromNullable,
+  tryCatch,
 }
